4389bc395b888f5c898671d8c2cb83e568dccb44
Ornella-e <orneescaladacorsi@gmail.com>
2022-07-26 17:54:31 +0200

    navbar options and logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,25 +1,28 @@
 import { NavLink } from "react-router-dom"
 import useAuth from "../../context/auth/useAuth"
 import "./Navbar.css"
+import logo from "./logo-option4.png"
 
 const Navbar = () => {
 	const { isLoggedIn, currentUser, removeUser } = useAuth()
 	return (
 		<nav className="Navbar">
-			<NavLink className="logo" to="/">
-				<img src="" alt=""/>
+			<NavLink className="logoImg" to="/">
+				<img src={logo} alt="logo"/>
 			</NavLink>
+			<NavLink className="sign" to="/" >Home</NavLink>
+			<NavLink className="sign" to="/about" >About</NavLink>
 			{isLoggedIn && (
 				<>
-				    <NavLink to="/about" >About</NavLink>
+				    
 					<NavLink to="/profile" >{currentUser.email}</NavLink>
 					<button onClick={removeUser}>Log out</button>
 				</>
 			)}
 			{!isLoggedIn && (
 				<>
-					<NavLink to="/signin" >Log in</NavLink>
-					<NavLink to="/signup" >Sign up</NavLink>
+					<NavLink className="sign" to="/signin" >Log in</NavLink>
+					<NavLink className="sign" to="/signup" >Sign up</NavLink>
 				</>
 			)}
 			<div className="search">
