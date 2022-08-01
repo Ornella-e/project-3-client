843050b803159fe15bb03fd2c08fafd1ab3139fd
Ornella-e <orneescaladacorsi@gmail.com>
2022-08-01 16:07:00 +0200

    reservation dates

--- src/pages/DetailsCouch.jsx
+++ src/pages/DetailsCouch.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import CouchItem from '../components/Couch/CouchItem'
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useState, useEffect} from "react";
+import { useParams, useNavigate  } from "react-router-dom";
 import axios from "axios";
+import service from '../services/apiHandler';
+import useAuth from '../context/auth/useAuth';
 
 export default function DetailsCouch() {
     const [couch, setCouch] = useState(null);
 	const { id } = useParams();
+  const [startingDate, setStartingDate] = useState("")
+  const [endingDate, setEndingDate] = useState("")
+	const navigate = useNavigate()
+	const {username} = useAuth()
+	console.log(username)
 
 	useEffect(() => {
 		axios
@@ -19,6 +26,34 @@ export default function DetailsCouch() {
 			.catch((e) => console.log(e));
 	}, [id]);
 
+ 
+	
+	const handleSubmit = async (e) => {
+		e.preventDefault()
+
+	
+		const fd = new FormData()
+
+    fd.append("usename", username)
+		fd.append("startingDate", startingDate)
+		fd.append("endingDate", endingDate)
+
+		try {
+
+			await service.post('/couch', fd)
+			navigate("/reservations")
+
+		} catch (error) {
+			console.error(error)
+		}
+	}
+
+    
+
+    
+	
+
+	
   return (
     <>
     {couch ? (
@@ -26,19 +61,58 @@ export default function DetailsCouch() {
       <div className='couch' >
  
  <img src={couch.image} alt="couchImg" />
- <p>user: {couch.owner}</p>
+ <p>user: {couch.username}</p>
  <p>Title: {couch.title}</p>
  <p>Description: {couch.description}</p>
  <p>Location: {couch.location.country}</p>
  <p>Location: {couch.location.city}</p>
  <p>Evaluations: {couch.evaluations}</p>
  <p>Calendar: {couch.calendar}</p>
- 
 </div>
+<form className="FormCouch" onSubmit={handleSubmit}>
+       <div>
+	   <label htmlFor="username">User: </label>
+				<input
+				type="text"
+				id="username"
+				name="username"
+      value={username}
+	  readOnly={username}
+	  />
+
+    </div>
+     
+	  <div>
+				<label htmlFor="startingDate">Start-Day: </label>
+				<input
+					type="date"
+					id="startingDate"
+					name="startingDate"
+					value={startingDate}
+					onChange={(e) => setStartingDate(e.target.value)}
+				/>
+			</div>
+      <div>
+				<label htmlFor="endingDate">End-Day: </label>
+				<input
+					type="date"
+					id="endingDate"
+					name="endingDate"
+					value={endingDate}
+					onChange={(e) => setEndingDate(e.target.value)}
+				/>
+			</div>
+			<button>Reserve</button>
+		</form>
+
         </>
+        
     ) : (
         <p>No couch</p>
     )}
+    
+    
 </>
+
   )
 }
--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
-export default function MyReservations() {
+export default function MyReservations(props) {
   return (
     <div>
       <h1>Details of my reservations</h1>
+      <p> Couch: {props._id}</p>
+      <p>Location: {props.location}</p>
+      <p> Start-Date: {props.startingDate}</p>
+      <p> End-Date: {props.endingDate}</p>
     </div>
   )
 }
