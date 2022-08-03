75f40bc5656fca4ca64b0b7b3c19e2422b0d0820
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-08-03 22:12:02 +0200

    Picture Displaying fixerd

--- src/components/User/ProfileCard.jsx
+++ src/components/User/ProfileCard.jsx
@@ -9,8 +9,8 @@ export default class ProfileCard extends Component {
           <h2>ProfileCard</h2>
           <h4>Identity verification</h4>
           <ProfileImage/>
-          <UserPictureUploader/>
-          <h4>Update Image</h4>
+          {/* <UserPictureUploader/> */}
+          <h4>Update/Change Image</h4>
           <h4>Become a Host</h4>
           
       </div>
