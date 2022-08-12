a023ef7916d7709b30d91212fbed02518a3a2bfc
Ornella-e <orneescaladacorsi@gmail.com>
2022-08-12 11:02:54 +0200

    reservation component

--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,26 +1,32 @@
 import React from "react";
 import Ranking from "../components/Ranking/Ranking";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import useAuth from "../context/auth/useAuth";
 import service from "../services/apiHandler";
+import axios from "axios";
+import Reservations from "../components/Reservation/Reservations";
 
 export default function MyReservations() {
   const { id } = useParams();
   const [couches, setCouches] = useState([]);
   const [username, setUsername] = useState("");
   const [reservedCouch, setReservedCouch] = useState([]);
-   jconst [evaluations, setEvaluations] = useState("");
+  const [evaluations, setEvaluations] = useState("");
   const navigate = useNavigate();
   const { currentUser } = useAuth();
   console.log(currentUser);
 
+
+  
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const fd = new FormData();
 
     fd.append("evaluations", evaluations);
+
+   
 
     try {
       await service.put(`/couch/${id}`, fd);
@@ -32,20 +38,9 @@ export default function MyReservations() {
 
   return (
     <>
-      <h1>Details of my reservations</h1>
-      {reservedCouch ? (
-        <>
-          <div className="couch">
-            <h2>Id: {reservedCouch._id}</h2>
-            <p>user: {reservedCouch.username}</p>
-            <p>check in: {reservedCouch.startingDate}</p>
-            <p>check out: {reservedCouch.endingDate}</p>
-          </div>
-        </>
-      ) : (
-        <p>No reservation</p>
-      )}
+      
       <div>
+      <Reservations/>
         <h2>Rate your stay at {reservedCouch.username}'s couch </h2>
         <Ranking />
       </div>
