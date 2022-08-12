97fa3be29fd6070e389e48c146ca341e08a0bf54
Ornella-e <orneescaladacorsi@gmail.com>
2022-08-12 11:33:23 +0200

    reservations page

--- src/components/Reservation/Reservations.jsx
+++ src/components/Reservation/Reservations.jsx
@@ -1,36 +1,17 @@
 import React from 'react'
 
-import { useState, useEffect } from "react";
 
-
-import axios from "axios";
-
-export default function Reservations() {
-  const [reservations, setReservations] = useState([]);
+export default function Reservations({user, startingDate, endingDate}) {
   
-  const getAllReservations = async () => {
-    const response = await axios.get(`${process.env.REACT_APP_API_URL}/couch/reservations`)
-     console.log(response)
-    setReservations(response.data)
-  }
-  useEffect(() => {
-    getAllReservations()
-  }, [])
 
   return (
     <div className="container">
-				{reservations.map((rent) => {
-					console.log(rent)
-					return (
-                        <div>
-                            key={rent._id}
-							id={rent._id}
-                        <p>user: {rent.username}</p>
-                        <p>check in: {rent.startingDate}</p>
-                        <p>check out: {rent.endingDate}</p>
-                        </div>
-					)
-				})}
+				
+                     {user}
+                     {startingDate}
+                    {endingDate}
+                    
+					
 			</div>
   )
   
--- src/pages/MyReservations.jsx
+++ src/pages/MyReservations.jsx
@@ -1,12 +1,38 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import axios from "axios";
 import Reservations from "../components/Reservation/Reservations";
 
 export default function MyReservations() {
 
+  const [reservations, setReservations] = useState([]);
+  
+  const getAllReservations = async () => {
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}/couch/reservations`)
+     console.log(response)
+    setReservations(response.data)
+  }
+  useEffect(() => {
+    getAllReservations()
+  }, [])
+
   return (
-      <Reservations/>
-     
-      
-  );
+    <div className="container">
+				{reservations.map((rent) => {
+					console.log(rent)
+					return (
+                        <Reservations
+                            key={rent._id}
+							              id={rent._id}
+                        user= {rent.username}
+                        check-in= {rent.startingDate}
+                        check-out= {rent.endingDate}
+                        />
+					)
+				})}
+			</div>
+  )
+  
 }
+
+
