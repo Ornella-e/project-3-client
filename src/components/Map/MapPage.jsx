d46b37cd79ab6550561243d6febf67a33bcf4d5b
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-08-15 17:51:51 +0200

    updade

---
+++ src/components/Map/MapPage.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import "./Map.css"
+
+function MapPage() {
+    const { isLoaded } = useJsApiLoader({
+        id: 'google-map-script',
+        googleMapsApiKey: "AIzaSyBAOLd6yXR--lNpR3z73Pvf5S0EXu_fIg4"
+    })
+    const position ={
+        lat:52.520008,
+        lng:13.404954
+    } 
+  return (
+    <div className='map'>
+        {isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={{widht: '100%', height: '100%'}}
+              center={position}
+              zoom={10}
+            >
+            <Marker position={position}/>
+             
+            </GoogleMap>
+        ) : <></>}
+        </div>
+  )
+}
+
+export default MapPage
