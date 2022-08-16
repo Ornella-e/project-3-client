be6da9985680a2ad8cbe6e81a2e6858429839b1d
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-08-16 12:09:50 +0200

    signup css done

--- src/components/Forms/FormSignUp.jsx
+++ src/components/Forms/FormSignUp.jsx
@@ -4,10 +4,6 @@ import service from "../../services/apiHandler"
 import { Link } from "react-router-dom"
 /* import CountrySelector from "./FormCountrySelector" */
 
-
-
-
-
 const FormSignUp = () => {
 
 	const [user, setUser] = useState({ username: "", email: "", location:"country", location:"city" , userImage:""/* ,couch: "" */  })
@@ -36,77 +32,108 @@ const FormSignUp = () => {
 			setError(e.message)
 		}
 	}
+	
 	return (
-		<>
-			{error && <h3 classusername="error">{error.message}</h3>}
-			<form onSubmit={handleSubmit}>
-				<h2>Signup</h2>
-				<label htmlFor="username"><p>Username</p></label>
-				<input
-					onChange={(e) =>
-						setUser({ ...user, [e.target.name]: e.target.value })
-					}
-					value={user.username}
-					type="text"
-					id="username"
-					name="username"
-				/>
-				<label htmlFor="email"><p>Email</p></label>
-				<input
-					onChange={(e) =>
-						setUser({ ...user, [e.target.name]: e.target.value })
-					}
-					value={user.email}
-					type="email"
-					id="email"
-					name="email"
-				/>
-				 <label htmlFor="password"><p>Password</p></label>
-				<input
-					onChange={(e) =>
-						setUser({ ...user, [e.target.name]: e.target.value })
-					}
-					value={user.password}
-					type="password"
-					id="password"
-					name="password"
-				/> 
+	<div className="Auth-form-container">
+		{error && <h3 classusername="error">{error.message}</h3>}
+		<form className="Auth-form" >
+			<div className="Auth-form-content">
+				<h2 className="Auth-form-title">Signup</h2>
+				<div className="text-center">
+						Already registered?{" "}
+					<span className="link-primary" >
+					<Link to={"/signin"} className="Sign">Signin</Link>
+					</span>
+				</div>
+				<div className="form-group mt-3">
+					<label htmlFor="username"><p>Username</p></label>
+					<input
+						onChange={(e) =>
+							setUser({ ...user, [e.target.name]: e.target.value })
+						}
+						className="form-control mt-1"
+						placeholder="Username"
+						value={user.username}
+						type="text"
+						id="username"
+						name="username"
+					/>
+				</div>
+				<div className="form-group mt-3">
+					<label htmlFor="email"><p>Email</p></label>
+					<input
+						onChange={(e) =>
+							setUser({ ...user, [e.target.name]: e.target.value })
+						}
+						value={user.email}
+						className="form-control mt-1"
+						placeholder="Email Address"
+						type="email"
+						id="email"
+						name="email"
+					/>
+				</div>
+				<div className="form-group mt-3">
+					<label htmlFor="password"><p>Password</p></label>
+					<input
+						onChange={(e) =>
+							setUser({ ...user, [e.target.name]: e.target.value })
+						}
+						value={user.password}
+						type="password"
+						className="form-control mt-1"
+						placeholder="Password"
+						id="password"
+						name="password"
+					/> 
+				</div>
 						
 					 {/* <CountrySelector/>  */}
-
-			 	<label htmlFor="country"><p>Country</p> </label>
-				 <input
-					onChange={(e) =>
-						setUser({ ...user, [e.target.name]: e.target.value })
-					}
-					value={user.location.country}
-					type="country"
-					id="country"
-					name="country"
-				/> 
-
-				 <label htmlFor="city"><p>Select your City</p> </label>
-				 <input
-					 onChange={(e) =>
-						 setUser({ ...user, [e.target.name]: e.target.value })
-					 }
-					 value={user.location.city}
-					 type="city"
-					 id="city"
-					 name="city"
-				 />  
-
- 
+				<div className="form-group mt-3">
+					<label htmlFor="country"><p>Country</p> </label>
+					<input
+						onChange={(e) =>
+							setUser({ ...user, [e.target.name]: e.target.value })
+						}
+						value={user.location.country}
+						className="form-control mt-1"
+						placeholder="Country"
+						type="country"
+						id="country"
+						name="country"
+					/> 
+				</div>
+				<div className="form-group mt-3">
+					<label htmlFor="city"><p>Select your City</p> </label>
+					<input
+						onChange={(e) =>
+							setUser({ ...user, [e.target.name]: e.target.value })
+						}
+						value={user.location.city}
+						className="form-control mt-1"
+						placeholder="City"
+						type="city"
+						id="city"
+						name="city"
+					/>  
+				 </div>
+				 <div className="form-group mt-3">
 					<label htmlFor="userImage"><p>userImage</p></label>
-				<input
-					
-					onChange={(e) =>
-						setUser({ ...user, [e.target.name]: e.target.files[0] })
-					}
-					type="file"
-					id="userImage"
-					name="userImage"
-				/> 
+					<input
+						
+						onChange={(e) =>
+							setUser({ ...user, [e.target.name]: e.target.files[0] })
+						}
+						type="file"
+						className="form-control mt-1"
+						id="userImage"
+						name="userImage"
+					/> 
+				</div>
+				<div className="d-grid gap-2 mt-3">
+				<Link to={"/signin"} className="btn btn-primary">Submit</Link>
+				
+				</div>
 			 
 					
 			{/* 	<label htmlFor="couch"><p>couch</p></label>
@@ -118,20 +145,16 @@ const FormSignUp = () => {
 					type="couch"
 					id="couch"
 					name="couch"
-<<<<<<< HEAD
 					/> 
 				<button>Create Account</button>
-=======
 					/>  */}
-					<p>
-				<button>Create Account</button>
-					</p>
->>>>>>> e8b358c7c41c8e41cc036315487e0c9a33d529ca
 				
-				<p>Do you have already an account?</p>
-				<Link to={"/signin"} className="Sign">Signin</Link>
-			</form>
-		</>
+				
+				{/* <p>Do you have already an account?</p>
+				<Link to={"/signin"} className="Sign">Signin</Link> */}
+			</div>	
+		</form>
+	</div>
 	)
 }
 
