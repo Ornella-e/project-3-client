bba90a356abd102cc7daa7c306feeac051bbbec1
DantasBeyer <100128679+DantasBeyer@users.noreply.github.com>
2022-08-19 15:54:29 +0200

    update

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import Search from "../components/Search/Search";
-import Map from "../components/Map/Map";
 import  "../styles/App.css"
-import CouchCard from "../components/CouchCards/CouchCard";
+import WebFont from 'webfontloader';
 
 
 
 const Home = () => {
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: ['Lobster']
+      }
+    });
+   }, []);
   const [couches, setCouches] = useState([]);
   const [searchedString, setSearchedString] = useState("")
 
@@ -35,56 +40,21 @@ const Home = () => {
     <div className="ListCouches">
       <div className="Container-Intro">
         <div className="text-intro">
-          <h1>Welcome to ShareCouch</h1>
-          <h3>
+          <div style={{fontFamily: 'Lobster'}}>
+          <h1 className="Title">ShareCouch</h1>
+          </div>
+          <h5 >
             The platform that connects travelers around the world looking for
             free accommodation, discover new places and make new friendships.
-          </h3>
+          </h5>
         </div>
-        <div className="text-intro"> 
-          <Search
-            searchedString={searchedString}
-            setSearchedString={setSearchedString}
-            couches={searchedCouches}/>
-           {searchedCouches.map((couch) => {
-          console.log(couch);
-        return (
-        
-          <Link to={couch._id}>
-            <ul class="cards">
-                <li>
-                    <a href="" class="card">
-                        <img  src={couch.image} class="card__image" />
-                        <div class="card__overlay">
-                        <div class="card__header">
-                            <svg class="card__arc" xmlns="http://www.w3.org/2000/svg"><path /></svg> 
-
-                            {/* user img */}
-                            {/* <img class="card__thumb" src="https://i.imgur.com/sjLMNDM.png" alt="" />  */}                   
-                            <div class="card__header-text">
-                                  <h3 class="card__title">{couch.title}</h3>
-                                  {/* <span class="card__status"> "Text"</span> */}   
-                            </div>
-                        </div>
-                        {/* <img className="image-home" src="{currentUser.userImage}" alt="home-img" /> */}
-                            <p class="card__description">{couch.location.country}</p>
-                            <p class="card__description">{couch.location.city}</p>
-                        </div>
-                    </a>      
-                </li>
-            </ul>  
-            </Link>
-            
-        );
-        })}  
-   
-        </div>
+       
 
         <div className="pic-intro">
           <p>Photo by Bao Menglong</p>
         </div>
       </div>
-      
+       
       <div className="couchCards">
         
       <div className='body'>
